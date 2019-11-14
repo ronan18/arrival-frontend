7826e670ed624cca8f40ceb4d82697aaf26f1412
Ronan <rsf.sho@gmail.com>
2019-11-14 09:31:49 -0800

    caching

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+console.log('Hello from service-worker.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+  workbox.routing.registerRoute(
+    'https://api.arrival.city/api/v2/stations',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'station-cache',
+    })
+  );
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    // Cache image files.
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    // Use the cache if it's available.
+    new workbox.strategies.CacheFirst({
+      // Use a custom cache name.
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images.
+          maxEntries: 20,
+          // Cache for a maximum of a week.
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
+    })
+  );
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -31,3 +31,9 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 */
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
